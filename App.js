209bb4a448d5de262fda65e4c3f4d7ba37a3c83c
Ahvko <t1ahko00@students.oamk.fi>
2024-03-24 00:13:24 +0200

    mapview 2 main

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, Platform, AppRegistry, StatusBar } from "react-native";
+import Map from "./screens/Map";
 
 export default function App() {
+
+  const [location, setLocation] = useState({
+    latitude: 65.0800,
+    longitude: 25.4800,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Map />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
 });
+
+AppRegistry.registerComponent('main', () => App);
